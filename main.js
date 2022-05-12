f546416196e0e820d5288f20da8c7be133478b83
dewinura <dewi2000016042@webmail.uad.ac.id>
2022-05-12 13:16:26 +0700

    ubah file

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ Vue.createApp({
       header: {
         title: "Dewi Nur Apriliyani",
         description:
-          "Let's be your self",
+          "My name is Dewi Nur Apriliyani a student of Ahmad Dahlan University Yogyakarta, born in Klaten on April 1, 2002",
         social: {
           ig: {
             url: "https://www.instagram.com/dewinaa.__/",
@@ -21,21 +21,15 @@ Vue.createApp({
         },
         articles1: 
         {
-         title: 'WHAT ABOUT ME ?',
-         description: "Nama saya Dewi Nur Apriliyani, lahir di Klaten pada tanggal 1 April 2002. Saya anak kedua dari dua bersaudara. Ibu saya adalah seorang guru dan ayah saya adalah seorang pegawai swasta. Saya memiliki seorang kakak perempuan, dia seorang guru. Saat ini saya adalah mahasiswi semester 4 program studi Sistem Informasi Universitas Ahmad Dahlan.",
-         thumbnail: 'image/foto.jpg'
-       },
-        articles2: 
-        {
           description: "SD Muhammadiyah Wedi adalah sebuah satuan pendidikan setingkat SD di Kalitengah, Wedi, Kabupaten Klaten, Jawa Tengah.",
           thumbnail: 'image/sd.png'
         },
-        articles3: 
+        articles2: 
         {
           description: "SMP Negeri 1 Wedi adalah Sekolah Menengah Pertama Negeri yang terletak di Kabupaten Klaten dengan alamat Sukorejo, Wedi, Klaten.",
           thumbnail: 'image/smp1.jpg'
         },
-        articles4: 
+        articles3: 
         {
           description: "SMA Negeri 1 Jogonalan adalah sebuah satuan pendidikan setingkat SMA di Prawatan, Jogonalan, Kabupaten Klaten, Jawa Tengah.",
           thumbnail: 'image/sma.jpg'
