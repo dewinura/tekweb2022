ec4b07d5865f17b4cbfa100aa2c8493dba8051d3
dewinura <dewi2000016042@webmail.uad.ac.id>
2022-05-12 11:28:34 +0700

    ubah file

--- get_json.js
+++ get_json.js
@@ -1,37 +1,54 @@
 Vue.createApp({
-    data() {
-      return {
-        header: {},
-        articles: [],
-      };
-    },
-    methods: {
-      getHeaderData() {
-        axios
-          .get('https://raw.githubusercontent.com/dewinura/tekweb2022/main/contents/header.json')
-          .then((res) => {
-            console.log(res.data);
-            this.header = res.data;
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      },
-      getArticlesData() {
-        axios
-          .get('https://raw.githubusercontent.com/dewinura/tekweb2022/main/contents/articles.json')
-          .then((res) => {
-            console.log(res.data);
-            this.articles = res.data;
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      },
-    },
-    beforeMount() {
-      this.getHeaderData();
-      this.getArticlesData();
-    },
-  }).mount('#app');
-  
+  data() {
+    return {
+      message: "Hello Vue!",
+      header: {},
+    };
+  },
+  methods: {
+    getHeaderData()
+    {
+      axios
+        .get(
+          "Vue.createApp({
+            data() {
+              return {
+                message: "Hello Vue!",
+                header: {},
+              };
+            },
+            methods: {
+              getHeaderData()
+              {
+                axios
+                  .get(
+                    "https://raw.githubusercontent.com/faridsurya/Vue-Axios-Basic/master/contents/header.json"
+                  )
+                  .then((res) => {
+                    console.log(res.data); //melihat respon data pada console browser
+                    this.header = res.data; //memperbarui variabel header pada bagian data()
+                  })
+                  .catch((error) => {
+                    console.log(error); //melihat error jika pengambilan data adalah gagal
+                  });
+              }
+              getArticlesData()
+              {
+                axios
+                .get(
+                  "https://raw.githubusercontent.com/dewinura/tekweb2022/main/contents/articles.json"
+                )
+                .then((res) => {
+                    console.log(res.data); //melihat respon data pada console browser
+                    this.header = res.data; //memperbarui variabel header pada bagian data()
+                  })
+                .catch((error) => {
+                    console.log(error); //melihat error jika pengambilan data adalah gagal
+                  });
+              }
+            },
+            beforeMount() {
+              this.getHeaderData() //eksekusi fungsi getHeaderData() pada bagian methods saat halaman terbuka
+              this.getArticlesData()
+            },
+          }).mount("#app");"
