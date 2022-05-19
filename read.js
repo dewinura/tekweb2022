b346a038a184da3ffeece7f5c3f311ddf474c587
dewinura <dewi2000016042@webmail.uad.ac.id>
2022-05-19 19:25:50 +0700

    ubah file

--- read.js
+++ read.js
@@ -1,23 +1,23 @@
 Vue.createApp({
     data() {
       return {
-        articles: null,
+        article: null,
       };
     },
     methods: {
       getMarkdownData() {
         const queryString = window.location.search;
         const urlParams = new URLSearchParams(queryString);
-        const articles = urlParams.get('articles');
+        const article = urlParams.get('article');
         var converter = new showdown.Converter();
-        console.log(articles);
+        console.log(articls);
         axios
           .get("https://raw.githubusercontent.com/dewinura/tekweb2022/main/contents/"+
-          articles
+          article
           )
           .then((res) => {
             var html = converter.makeHtml(res.data);
-            this.articles = html;
+            this.article = html;
             console.log(html);
           })
           .catch((error) => {
